7eef58d598c51a6bb476fe85afabe5dfbd1a600c
junseopkim <junseop.kim@saltlux.com>
2023-01-10 08:35:41 +0900

    commitpractice

---
+++ src/main/java/com/example/project_toy/product/dto/MemberDTO.java
@@ -0,0 +1,31 @@
+package com.example.project_toy.product.dto;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class MemberDTO {
+
+    @NotNull
+    private String memberId;
+
+    @NotNull
+    @Id
+    private String memberName;
+
+    @NotNull
+    private String memberMail;
+
+    @NotNull
+    private Date memberRegdate;
+
+    @NotNull
+    private String memberGrade;
+}
