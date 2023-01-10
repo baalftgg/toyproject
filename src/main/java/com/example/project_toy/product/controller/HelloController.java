7c0090413c93e4ae9fc2f33688dc433eefc41132
junseopkim <junseop.kim@saltlux.com>
2023-01-10 09:50:18 +0900

    hellotest

--- src/main/java/com/example/project_toy/product/controller/HelloController.java
+++ src/main/java/com/example/project_toy/product/controller/HelloController.java
@@ -2,16 +2,16 @@ package com.example.project_toy.product.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
-    @RequestMapping("/hello")
+//    private final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+    @GetMapping("/hello")
     public String hello(){
-        return "Hello world!";
+        return "안녕!";
     }
 
 //    @PostMapping("log-test")
